ea83608d569bd82d0af06737c2f60a21f08a375e
pesekon2 <pesej.ondrek@gmail.com>
2021-05-13 17:27:58 +0300

    change indentation to two spaces in download-results.sh

--- scripts/download-results.sh
+++ scripts/download-results.sh
@@ -1,33 +1,33 @@
 Help()
 {
-   # Display Help
-   echo "*****"
-   echo "download-results.sh"
-   echo "Download all Venus data from the link the user has been provided."
-   echo
-   echo "Syntax: download-results.sh [-h] url username"
-   echo "options:"
-   echo "url         The url the user has obtained from the Venus request"
-   echo "username    The username required to obtain the data"
-   echo "h           Print this help and exit"
-   echo "*****"
+  # Display Help
+  echo "*****"
+  echo "download-results.sh"
+  echo "Download all Venus data from the link the user has been provided."
+  echo
+  echo "Syntax: download-results.sh [-h] url username"
+  echo "options:"
+  echo "url         The url the user has obtained from the Venus request"
+  echo "username    The username required to obtain the data"
+  echo "h           Print this help and exit"
+  echo "*****"
 }
 
 # check flags
 while getopts ":h" option; do
-   case "$option" in
-      h) # display Help
-         Help
-         exit;;
-     \?) # incorrect option
-         echo "ERROR: Invalid option"
-         echo
-         echo
-         echo "See help for the correct syntax:"
-         echo
-         Help
-         exit;;
-   esac
+  case "$option" in
+    h) # display Help
+      Help
+      exit;;
+    \?) # incorrect option
+      echo "ERROR: Invalid option"
+      echo
+      echo
+      echo "See help for the correct syntax:"
+      echo
+      Help
+      exit;;
+  esac
 done
 
 # parse arguments
