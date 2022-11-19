8deb9389a63d956c071ac07354daf84ef50ff60c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 04:31:51 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/filter-cloud-coverage.py
+++ scripts/filter-cloud-coverage.py
@@ -48,7 +48,26 @@ def filter_cc(data_dir: str,
                 # extract DBL and remove the archive
                 archive_path = os.path.join(data_dir, md_file[:-3] + 'DBL')
                 with tarfile.open(archive_path) as tar:
-                    tar.extractall(path=data_dir)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, path=data_dir)
                 os.remove(archive_path)
         elif delete is True:
             # remove metadata and the DBL archive of the scenes not meeting
